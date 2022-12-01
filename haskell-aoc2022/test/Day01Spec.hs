fa43685e917ce79cee54af8b50f32c1aafb6c5da
L Dean <lgdean@users.noreply.github.com>
2022-11-30 21:18:16 -0800

    test for Part 2

--- haskell-aoc2022/test/Day01Spec.hs
+++ haskell-aoc2022/test/Day01Spec.hs
@@ -15,3 +15,12 @@ spec = do
     it "can solve Part 1" $ do
       input <- readFile "inputs/day01"
       doPart1 input `shouldBe` 71300
+
+  describe "Part 2" $ do
+    it "can handle given example" $ do
+      input <- readFile "inputs/day01-example"
+      doPart2 input `shouldBe` 45000
+
+    it "can solve Part 2" $ do
+      input <- readFile "inputs/day01"
+      doPart2 input `shouldBe` 0
