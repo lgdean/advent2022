712947f9ae247cd34011274bc04d8f46778f738d
L Dean <lgdean@users.noreply.github.com>
2022-11-30 20:07:02 -0800

    add libs used last year, including my own

--- haskell-aoc2022/src/Lib.hs
+++ haskell-aoc2022/src/Lib.hs
@@ -1,6 +1,18 @@
 module Lib
-    ( someFunc
+    ( readIntLines
+    , fixedPoint
+    , bin2Int
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Char(digitToInt)
+
+readIntLines :: [Char] -> [Int]
+readIntLines = map read . lines
+
+fixedPoint :: Eq a => (a -> a) -> a -> a
+fixedPoint f initState =
+  let nextState = f initState
+  in if initState == nextState then nextState else fixedPoint f nextState
+
+bin2Int :: String -> Int
+bin2Int str = foldl (\acc n -> acc*2+n) 0 (map digitToInt str)
