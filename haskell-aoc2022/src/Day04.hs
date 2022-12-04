e65301e6ac7f6be3d1ebc58a469f738b0ba44ced
L Dean <lgdean@users.noreply.github.com>
2022-12-04 12:05:11 -0800

    add failing test for Day 4

---
+++ haskell-aoc2022/src/Day04.hs
@@ -0,0 +1,14 @@
+module Day04
+    (
+      doPart1,
+      doPart2
+    ) where
+
+doPart1 :: [Char] -> Int
+doPart1 input =
+  let allLines = lines input
+  in 0
+
+doPart2 :: [Char] -> Int
+doPart2 input =
+  0
---
+++ haskell-aoc2022/test/Day04Spec.hs
@@ -0,0 +1,17 @@
+module Day04Spec where
+{-# LANGUAGE OverloadedStrings #-}
+
+import Test.Hspec
+
+import Day04
+
+spec :: Spec
+spec = do
+  describe "Part 1" $ do
+    it "can handle given example" $ do
+      input <- readFile "inputs/day04-example"
+      doPart1 input `shouldBe` 2
+
+    it "can solve Part 1" $ do
+      input <- readFile "inputs/day04"
+      doPart1 input `shouldBe` 0
