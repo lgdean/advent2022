da15d27d414b8c007dff130d825c8928f61e809e
L Dean <lgdean@users.noreply.github.com>
2022-12-04 21:45:42 -0800

    day 5, part 1

--- haskell-aoc2022/src/Day05.hs
+++ haskell-aoc2022/src/Day05.hs
@@ -4,13 +4,35 @@ module Day05
       doPart2
     ) where
 
-import Data.List.Split(splitOn)
+import Data.Map (Map)
+import qualified Data.Map.Strict as Map
 
-doPart1 :: [Char] -> Int
-doPart1 input =
+type Move = (Int, Int, Int)
+type CrateArrangement = Map Int [Char]
+
+doMove :: Move -> CrateArrangement -> CrateArrangement
+doMove (howMany, src, dest) curr =
+  let (toMove, toKeep) = splitAt howMany (curr Map.! src)
+  in Map.adjust (addCrates toMove) dest $ Map.insert src toKeep curr
+
+addCrates :: [a] -> [a] -> [a]
+addCrates [] old = old
+addCrates (x:xs) old = addCrates xs (x:old)
+
+doPart1 :: [(Int, [Char])] -> [Char] -> [Char]
+doPart1 startState input =
   let allLines = lines input
-  in 0
+      moves = map parseMove allLines
+      cratesStart = Map.fromList startState
+      cratesEnd = foldl (flip doMove) cratesStart moves
+  in map head $ Map.elems cratesEnd
 
 doPart2 :: [Char] -> Int
 doPart2 input =
   0
+
+parseMove :: String -> Move
+parseMove move =
+  case words move of
+    [_, n, _, src, _, dest] -> (read n, read src, read dest)
+    _ -> error ("failed to parse; " ++ move)
--- haskell-aoc2022/test/Day05Spec.hs
+++ haskell-aoc2022/test/Day05Spec.hs
@@ -9,14 +9,24 @@ spec :: Spec
 spec = do
   describe "Part 1" $ do
     it "can handle given example" $ do
-      pending
       input <- readFile "inputs/day05-example"
-      doPart1 input `shouldBe` undefined
+      let startState = [ (1, ['N','Z']), (2, ['D','C', 'M']), (3, ['P'])]
+      doPart1 startState input `shouldBe` "CMZ"
 
     it "can solve Part 1" $ do
-      pending
       input <- readFile "inputs/day05"
-      doPart1 input `shouldBe` 0
+      let startState = [
+                         (1, ['D','H','R','Z','S','P','W','Q'])
+                       , (2, ['F','H','Q','W','R','B','V'])
+                       , (3, ['H','S','V','C'])
+                       , (4, ['G','F','H'])
+                       , (5, ['Z','B','J','G','P'])
+                       , (6, ['L','F','W','H','J','T','Q'])
+                       , (7, ['N','J','V','L','D','W','T','Z'])
+                       , (8, ['F','H','G','J','C','Z','T','D'])
+                       , (9, ['H','B','M','V','P','W'])
+                      ]
+      doPart1 startState input `shouldBe` "ZWHVFWQWW"
 
   describe "Part 2" $ do
     it "can handle given example" $ do
