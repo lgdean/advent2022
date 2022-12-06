a255bd779fe76e0ce9ac7886b7ff44354d74e168
L Dean <lgdean@users.noreply.github.com>
2022-12-05 21:44:54 -0800

    refactor: move part1Inner into general findMarker function

--- haskell-aoc2022/src/Day06.hs
+++ haskell-aoc2022/src/Day06.hs
@@ -8,13 +8,15 @@ import Data.Set ()
 import qualified Data.Set as Set
 
 doPart1 :: [Char] -> Int
-doPart1 = part1Inner 4
+doPart1 = findMarker 4
 
-part1Inner :: Ord a => Int -> [a] -> Int
-part1Inner pos input =
-  let first4 = take 4 input
-      set4 = Set.fromList first4
-  in if Set.size set4 == 4 then pos else part1Inner (pos+1) (tail input)
+findMarker :: Ord a => Int -> [a] -> Int
+findMarker howMany input =
+  let part1Inner pos rest =
+        let first4 = take howMany rest
+            set4 = Set.fromList first4
+        in if Set.size set4 == howMany then pos else part1Inner (pos+1) (tail rest)
+  in part1Inner howMany input
 
 doPart2 :: [Char] -> Int
 doPart2 = part2Inner 14
