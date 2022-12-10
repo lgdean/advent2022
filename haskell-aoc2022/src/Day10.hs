c4973b38d258fda64afd5ec19e9aa50df8fe968d
L Dean <lgdean@users.noreply.github.com>
2022-12-09 22:17:43 -0800

    day 10, part 2

--- haskell-aoc2022/src/Day10.hs
+++ haskell-aoc2022/src/Day10.hs
@@ -4,6 +4,7 @@ module Day10
       doPart2
     ) where
 
+import Data.List (intercalate)
 import Data.List.Split (chunksOf)
 
 import Debug.Trace (trace)
@@ -23,10 +24,19 @@ doPart1 input =
       -- this line started out simple when I first wrote it (incorrectly)
       everyTwentySignals = map last $ filter ((20 ==) . length) $ chunksOf 20 $ zip [1..] resultStream
       interestingSignals = map head $ chunksOf 2 everyTwentySignals
-  in trace (show interestingSignals) $ sum $ map (uncurry (*)) interestingSignals
+  in sum $ map (uncurry (*)) interestingSignals
 
-doPart2 :: String -> Int
-doPart2 input = 0
+doPart2 :: String -> String
+doPart2 input =
+  let allLines = lines input
+      program = map parseLine allLines
+      resultStream = 1 : runProgram 1 program
+      isVisible pos regX = abs (pos-regX) <= 1
+      crtRows = map (zipWith isVisible [0..]) $ chunksOf 40 resultStream
+      renderRow = map (\b -> if b then '#' else '.')
+      result = intercalate "\n" $ map renderRow crtRows
+--  in trace result result
+  in result
 
 parseLine :: String -> Instruction
 parseLine line =
