f1dce6352a8d35e1c63944e76121e51a74f9eb25
L Dean <lgdean@users.noreply.github.com>
2022-12-18 18:59:50 -0800

    Day 18, Part 2

--- haskell-aoc2022/src/Day18.hs
+++ haskell-aoc2022/src/Day18.hs
@@ -4,12 +4,11 @@ module Day18
       doPart2
     ) where
 
-import Data.List (groupBy, sort)
+import Data.List ((\\), groupBy, partition, sort)
 import Data.List.Split (splitOn)
 
 type Cube = (Int, Int, Int)
 
--- may not need it, but glad I thought it through
 adjacent :: Cube -> Cube -> Bool
 adjacent (a,b,c) (x,y,z) =
   let diffs = map abs [a-x, b-y, c-z]
@@ -40,16 +39,49 @@ nAdjacent xs =
   let p a b = a+1 == b
   in length $ filter id $ zipWith p xs (tail xs)
 
+part1SurfaceArea :: [Cube] -> Int
+part1SurfaceArea cubes =
+  let sharedSides = nAdjacentCubes cubes
+      totalSides = 6 * length cubes
+  in totalSides - 2 * sharedSides
+
 doPart1 :: String -> Int
 doPart1 input =
   let allLines = lines input
       cubes = map parseLine allLines
-      sharedSides = nAdjacentCubes cubes
-      totalSides = 6 * length cubes
-  in totalSides - 2 * sharedSides
+  in part1SurfaceArea cubes
 
 doPart2 :: String -> Int
-doPart2 _ = 0
+doPart2 input =
+  let allLines = lines input
+      cubes = map parseLine allLines
+      maxX = maximum $ map (\(x,_,_) -> x) cubes
+      maxY = maximum $ map (\(_,y,_) -> y) cubes
+      maxZ = maximum $ map (\(_,_,z) -> z) cubes
+      dropletAndAroundIt = [(x,y,z) | x <- [(-1)..maxX+1], y <- [(-1)..maxY+1], z <- [(-1)..maxZ+1]]
+      somePointOutsideDroplet = head dropletAndAroundIt
+      (_notPartOfDroplet, airInside) = collectClump [somePointOutsideDroplet] [] (tail dropletAndAroundIt \\ cubes)
+  in part1SurfaceArea cubes - part1SurfaceArea airInside
+
+_collectClumps :: [Cube] -> [[Cube]]
+_collectClumps [] = []
+_collectClumps (x:rest) =
+  let (firstClump, remaining) = collectClump [x] [] rest
+  in firstClump : _collectClumps remaining
+
+-- there are of course more efficient ways to do this collection!
+-- and I gather (haha) there are simpler ways to solve this problem, anyway.
+collectClump :: [Cube] -> [Cube] -> [Cube] -> ([Cube], [Cube])
+collectClump currGen prevGens [] = (currGen ++ prevGens, []) -- no more to collect? all done!
+collectClump [] prevGens candidates = (prevGens, candidates) -- no more to check? all done with this clump!
+collectClump currGen prevGens candidates =
+  let (nextGen, remainingCandidates) = foldl grabAndCombine ([], candidates) currGen
+  in collectClump nextGen (currGen ++ prevGens) remainingCandidates
+
+grabAndCombine :: ([Cube], [Cube]) -> Cube -> ([Cube], [Cube])
+grabAndCombine (found, candidates) curr =
+  let (newlyFound, remainingCandidates) = partition (adjacent curr) candidates
+  in (newlyFound ++ found, remainingCandidates)
 
 
 parseLine :: String -> Cube
--- haskell-aoc2022/test/Day18Spec.hs
+++ haskell-aoc2022/test/Day18Spec.hs
@@ -22,11 +22,22 @@ spec = do
 
   describe "Part 2" $ do
     it "can handle given example" $ do
-      pending
       input <- readFile "inputs/day18-example"
-      doPart2 input `shouldBe` undefined
+      doPart2 input `shouldBe` 58
+
+    it "can handle a solid cube" $ do
+      input <- readFile "inputs/day18-cube-solid"
+      doPart2 input `shouldBe` 150
+
+    it "can handle a hollow cube" $ do
+      input <- readFile "inputs/day18-cube"
+      doPart2 input `shouldBe` 150
+
+    it "can handle a hollow almost-cube (one piece missing from face)" $ do
+      input <- readFile "inputs/day18-cube-almost"
+      doPart2 input `shouldBe` 206
 
     it "can solve Part 2" $ do
-      pending
       input <- readFile "inputs/day18"
-      doPart2 input `shouldBe` 0
+      -- 1974 is too low, as is 1976
+      doPart2 input `shouldBe` 1980
