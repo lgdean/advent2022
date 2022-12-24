87c8cd9fce4c6855dce3ddf6e04d21b96b55966c
L Dean <lgdean@users.noreply.github.com>
2022-12-24 01:06:59 -0800

    failing test for part 2

--- haskell-aoc2022/test/Day24Spec.hs
+++ haskell-aoc2022/test/Day24Spec.hs
@@ -13,14 +13,14 @@ spec = do
       doPart1 input `shouldBe` 18
 
     it "can solve Part 1" $ do
+      pending
       input <- readFile "inputs/day24"
       doPart1 input `shouldBe` 247
 
   describe "Part 2" $ do
     it "can handle given example" $ do
-      pending
       input <- readFile "inputs/day24-example"
-      doPart2 input `shouldBe` undefined
+      doPart2 input `shouldBe` 54
 
     it "can solve Part 2" $ do
       pending
